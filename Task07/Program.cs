5d61bbc1ff99ce88ab7ec33083c50f224ed5b5c9
rusyun <rustam.yunusov@attolabs.de>
2023-01-14 16:07:47 +0500

    add Task03 Task05 Task07

---
+++ Task07/Program.cs
@@ -0,0 +1,18 @@
+﻿/*7. Напишите программу, которая принимает на вход трёхзначное число
+на выходе показывает последнюю цифру
+этого числа.
+456 -> 6
+782 -> 2
+918 -> 8*/
+
+Console.WriteLine("Введите трехзнаное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 1000)
+{
+  Console.WriteLine("Вы ввели четырёхзнаное число!");
+}
+else
+{
+  int lastDigit = number % 10;
+  Console.WriteLine($"Последняя цифра: {lastDigit}");
+}
