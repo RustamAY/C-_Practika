b3b8666ace50cb45c6a9fc7e14ba8cdbf8019deb
rusyun <rustam.yunusov@attolabs.de>
2023-01-14 15:33:03 +0500

    Initial commit

---
+++ Task00/Program.cs
@@ -0,0 +1,12 @@
+﻿//0. Демонстрация решения
+// Напишите программу, которая на вход
+// принимает число и выдаёт его квадрат (число умноженное на само себя).
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+Console.WriteLine("Insert a integer number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+Console.WriteLine($"Square of {number} is {square}");
---
+++ Task01/Program.cs
@@ -0,0 +1,21 @@
+﻿// 1. Напишите программу, которая на вход принимает два
+// числа и проверяет, является ли первое число квадратом
+// второго.
+// a = 25, b = 5 -> да
+// a = 2, b = 10 -> нет
+// a = 9, b = -3 -> да
+// a = -3 b = 9 -> нет
+
+Console.WriteLine("Inser number a:");
+int a = Convert.ToInt32(Console.ReadLine()); //25
+Console.WriteLine("Inser number b:");
+int b = Convert.ToInt32(Console.ReadLine()); // 2
+int square = b * b;
+if (a == square)
+{
+  Console.WriteLine($"Number {a} is square of {b}");
+}
+else
+{
+  Console.WriteLine($"Number {a} is not square of {b}");
+}
