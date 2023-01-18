9633539e89b72020d09f31bfc2fc4a42c4fe3624
rusyun <rustam.yunusov@attolabs.de>
2023-01-18 16:19:59 +0500

    add HW_Task15

---
+++ HW_Task15/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+//и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+bool CheckingWeekend(int day)
+{
+  return day == 6 || day == 7;
+}
+
+Console.Write("Введите цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+bool result = CheckingWeekend(dayNumber);
+
+if (dayNumber < 1 || dayNumber > 7) Console.WriteLine ("Вы ввели неправильную цифру!");
+else Console.WriteLine(result ? "Да" : "Нет");
