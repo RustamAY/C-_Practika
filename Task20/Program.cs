3c87bf5d78ebf56c20d8687325f9410a7c451b3b
rusyun <rustam.yunusov@attolabs.de>
2023-01-23 22:11:02 +0500

    update Task20

--- Task20/Program.cs
+++ Task20/Program.cs
@@ -21,27 +21,12 @@ Console.Write("Введите BY: ");
 int bY = Convert.ToInt32(Console.ReadLine());
 
 double result = GetCoordinates(aX, aY, bX, bY);
-Console.Write($"Растояние между точками: {result}");
+double resultRound = Math.Round(result, 2, MidpointRounding.ToZero);
+Console.Write($"Растояние между точками: {resultRound}");
 
-
-// Console.WriteLine("введите координаты первой точки ");
-// Console.Write("X1: ");
-// int x1 = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Y1: ");
-// int y1 = Convert.ToInt32(Console.ReadLine());
-
-// Console.WriteLine("введите координаты второй точки ");
-// Console.Write("X2: ");
-// int x2 = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Y2: ");
-// int y2 = Convert.ToInt32(Console.ReadLine());
-
-// double distance = Distance(x1, y1, x2, y2);
-// double distanceRound = Math.Round(distance, 2, MidpointRounding.ToZero);
-// Console.WriteLine($"Расстояние между точками {distanceRound}");
-
-
-// double Distance(int xc1, int yc1, int xc2, int yc2)
-// {
-//     return Math.Sqrt(Math.Pow((xc2 - xc1), 2) + Math.Pow((yc2 - yc1), 2));
-// }
+// double d = 5.09997623455;
+// double dRuynd = Math.Round(d, 2, MidpointRounding.ToZero);
+// System.Console.WriteLine(dRuynd);
+// Math.Pow (2, 10); - возведение в степень возврашает doubl
+// Math.Sqrt(5); - извлечение корня возврашает doubl
+// Math.Round(d, 2, MidpointRounding.ToZero) - округление до второго знака
