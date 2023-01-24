57dc51e4eaf64c60cb4f5f00fd9533dfb9f4fb04
rusyun <rustam.yunusov@attolabs.de>
2023-01-24 13:35:51 +0500

    Task26 and Task28

---
+++ Task26/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 26: Напишите программу, которая 
+// принимает на вход число и 
+// выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int NumberDigit(int num)
+{
+  int count = 0;
+  if (num <0) num=num*-1;
+  while(num >0)
+  {
+    num = num/10;
+    count++;
+  }
+  return count;
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = NumberDigit(number);
+Console.WriteLine(result);
---
+++ Task28/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+
+int Factor(int num)
+{
+  int factor = 1;
+  for (int i = 1; i <= num; i++)
+  {
+    factor *= i;
+  }
+  return factor;
+}
+
+Console.Write("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0) Console.WriteLine("Ввели не положительное число!");
+else
+{
+  int factorial = Factor(number);
+  Console.WriteLine($"Произведение чисел от 1 до {number} = {factorial}");
+}
