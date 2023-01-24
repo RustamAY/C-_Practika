79bfb4c497080980ee0387c84e5cc99c025462a9
rusyun <rustam.yunusov@attolabs.de>
2023-01-24 13:41:42 +0500

    update Task28

--- Task28/Program.cs
+++ Task28/Program.cs
@@ -1,6 +1,8 @@
 ﻿// Задача 28: Напишите программу, которая
-// принимает на вход число (А) и 
-// выдаёт произведения чисел.
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
 
 
 int Factor(int num)
@@ -15,7 +17,7 @@ int Factor(int num)
 
 Console.Write("Введите целое положительное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-if (number < 0) Console.WriteLine("Ввели не положительное число!");
+if (number < 0) Console.WriteLine("Не корректный ввод!");
 else
 {
   int factorial = Factor(number);
