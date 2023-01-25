cf1094e7c46f99aa80d0aaedc9a8b1e35d2ee5e8
rusyun <rustam.yunusov@attolabs.de>
2023-01-25 17:55:42 +0500

    add HW_Task25

---
+++ HW_Task25/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 25: Напишите цикл, который 
+// принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Pow(int num1, int num2)
+{
+  int result = 1;
+  for (int i = 0; i < num2; i++)
+  {
+    result = result * num1;
+  }
+  return result;
+}
+
+Console.Write("Введите число А: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное число В: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number2 < 0) Console.WriteLine("Введите натуральное число В!");
+else
+{
+  int pow = Pow(number1, number2);
+  Console.WriteLine($"Число {number1} в {number2} степени = {pow}");
+}
