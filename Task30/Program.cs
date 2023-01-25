e73fc8ef7dc4b3e1d62b2bbf14605baf2e2e5e38
rusyun <rustam.yunusov@attolabs.de>
2023-01-25 21:30:43 +0500

    update Task30

--- Task30/Program.cs
+++ Task30/Program.cs
@@ -4,24 +4,24 @@
 // [1,0,1,1,0,1,0,0]
 
 
-int[] GenerateArray(int size,int min, int max)
+int[] GenerateArray(int size, int min, int max)
 {
-    int[] arr = new int[size];
-    Random rnd = new Random(); 
-    for (int i = 0; i < arr.Length; i++)
-    {
-        arr[i] = rnd.Next(min, max + 1);
-    }
-    return arr;
+  int[] arr = new int[size];
+  Random rnd = new Random();
+  for (int i = 0; i < arr.Length; i++)
+  {
+    arr[i] = rnd.Next(min, max + 1);
+  }
+  return arr;
 }
 
 void PrintArray(int[] arr)
 {
-    for (int i = 0; i < arr.Length; i++)
-    {
-        Console.Write($"{arr[i]} " );
-    }
+  for (int i = 0; i < arr.Length; i++)
+  {
+    Console.Write($"{arr[i]} ");
+  }
 }
 
-int [] array = GenerateArray(8, 0, 1);
+int[] array = GenerateArray(8, 0, 1);
 PrintArray(array);
