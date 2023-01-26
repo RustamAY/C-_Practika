6c4c399c266652268ffbe39e3665a8fc8700535a
rusyun <rustam.yunusov@attolabs.de>
2023-01-26 14:26:59 +0500

    add HW_Task29

---
+++ HW_Task29/Program.cs
@@ -0,0 +1,30 @@
+﻿//  Напишите программу, которая задаёт массив из 8 элементов и 
+//  выводит их на экран.
+
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+int[] GenerateArray(int size, int min, int max)
+{
+  int[] arr = new int[size];
+  Random rnd = new Random();
+  for (int i = 0; i < arr.Length; i++)
+  {
+    arr[i] = rnd.Next(min, max + 1);
+  }
+  return arr;
+}
+
+void PrintArray(int[] arr)
+{
+  for (int i = 0; i < arr.Length; i++)
+  {
+    Console.Write($"{arr[i]}");
+    if (i < arr.Length-1) Console.Write(", ");
+  }
+}
+
+int[] array = GenerateArray(8, 10, 100);
+PrintArray(array);
+Console.Write(" -> [");
+PrintArray(array);
+Console.Write("]" );
