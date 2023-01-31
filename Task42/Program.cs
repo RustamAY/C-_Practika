3bdd2a3028f25fb7d9689ae99e6a887117410ee8
rusyun <rustam.yunusov@attolabs.de>
2023-01-31 13:27:20 +0500

    add Task42

---
+++ Task42/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 42. Напишите программу которая будет преобразовывать десятичное число в двоичное
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+string DecToBin(int num)
+{
+  string resNum = string.Empty;
+  while (num > 0)
+  {
+    resNum = num % 2+ resNum;
+    num = num / 2;
+  }
+  return resNum;
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string decToBin = DecToBin(number);
+Console.WriteLine(decToBin);
