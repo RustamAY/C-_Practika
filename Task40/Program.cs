ff9f748754cc2a27871898a53410b0f9c7adfce1
rusyun <rustam.yunusov@attolabs.de>
2023-01-31 13:26:56 +0500

    add Task40

---
+++ Task40/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 40. Напишите программу, которая, принимает на вход три числа
+// и проверяет, может, ли со сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника
+// меньше суммы двух других сторон
+
+bool CheckTringle (int side1, int side2, int side3)
+{
+  return side1<(side2+side3) && side2<(side1+side3) && side3<(side1+side2);
+}
+
+Console.Write("Введите первую сторону треугольника: ");
+int firstSide = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите вторую сторону треугольника: ");
+int secondSide = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третью сторону треугольника: ");
+int thirdSide = Convert.ToInt32(Console.ReadLine());
+bool result = CheckTringle(firstSide, secondSide, thirdSide);
+Console.WriteLine (result ? "Да" : "Нет");
