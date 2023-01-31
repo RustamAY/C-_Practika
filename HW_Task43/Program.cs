fc9d533177204dc85288339f69165641db14f423
rusyun8 <rustam.yunusov@attolabs.de>
2023-01-31 18:39:57 +0500

    add HW_Task43

---
+++ HW_Task43/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+double FindX(double b1, double k1, double b2, double k2)
+{
+  double x = Math.Round(((b2 - b1) / (k1 - k2)), 2);
+  return x;
+}
+
+double FindY(double b2, double k2, double x)
+{
+  double y = Math.Round((k2 * x + b2), 2);
+  return y;
+}
+
+
+Console.Write("Введите число для b1: ");
+double numB1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число для k1: ");
+double numK1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число для b2: ");
+double numB2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число для k2: ");
+double numK2 = Convert.ToInt32(Console.ReadLine());
+
+double x = FindX(numB1, numK1, numB2, numK2);
+double y = FindY(numB2, numK2, x);
+Console.Write($"({x}; {y})");
