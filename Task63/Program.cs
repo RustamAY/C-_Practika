2ae90b2c026579502a60e5f997fcbceb5546df9e
rusyun8 <rustam.yunusov@attolabs.de>
2023-02-10 12:47:50 +0500

    add task63

---
+++ Task63/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+// Метод с рекурсией
+void NaturalNumbers(int num)
+{
+  if (num == 0) return;
+  NaturalNumbers(num - 1);
+  Console.Write($"{num} ");
+}
+
+Console.WriteLine("Введите натуральное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+NaturalNumbers(number);
