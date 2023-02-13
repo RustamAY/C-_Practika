7c55b19ef00744552ca585912b794c017fac9d63
rusyun8 <rustam.yunusov@attolabs.de>
2023-02-13 21:36:18 +0500

    update hw task66

--- HW_Task66/Program.cs
+++ HW_Task66/Program.cs
@@ -7,16 +7,20 @@
 // M = 4; N = 8 -> 30
 
 int SumOfNaturalNumbers(int numM, int numN)
-{
-  if (numM>numN) return numM == numN ? numN : SumOfNaturalNumbers(numN+1, numM) + numN;
-  else return numM == numN ? numM : SumOfNaturalNumbers(numM+1, numN) + numM;
-
+{ 
+  return numM == numN ? numM : SumOfNaturalNumbers(numM + 1, numN) + numM;
 }
 
 Console.Write("Введите число M: ");
 int numberM = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число N: ");
 int numberN = Convert.ToInt32(Console.ReadLine());
+if (numberM > numberN)
+{
+  int temp = numberM;
+  numberM = numberN;
+  numberN = temp;
+}
 if (numberM < 0 || numberN < 0) Console.WriteLine("Вы ввели отрицательное число!");
 else
 {
