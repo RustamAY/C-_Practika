67509e6f721e0057a5da50574b658afb7e3d587e
rusyun8 <rustam.yunusov@attolabs.de>
2023-02-13 21:22:37 +0500

    add hw task66

---
+++ HW_Task66/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 66: 
+// 1. Задайте значения M и N. 
+// 2. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. 
+// 3. Выполнить с помощью рекурсии.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+int SumOfNaturalNumbers(int numM, int numN)
+{
+  if (numM>numN) return numM == numN ? numN : SumOfNaturalNumbers(numN+1, numM) + numN;
+  else return numM == numN ? numM : SumOfNaturalNumbers(numM+1, numN) + numM;
+
+}
+
+Console.Write("Введите число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+if (numberM < 0 || numberN < 0) Console.WriteLine("Вы ввели отрицательное число!");
+else
+{
+  int sum = SumOfNaturalNumbers(numberM, numberN);
+  Console.Write($"M = {numberM}; N = {numberN} -> {sum}");
+}
